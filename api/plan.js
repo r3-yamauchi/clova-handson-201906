167e3b66e9e0ede3bbeb93743caabbc58d40d962
EC2 Default User <ec2-user@ip-172-31-39-208.ap-northeast-1.compute.internal>
2019-05-28 17:31:07 +0000

    planIdを条件にfilterするように変更

--- api/plan.js
+++ api/plan.js
@@ -4,9 +4,9 @@ const jsonData = require('../data.json');
 
 module.exports = ( req, res ) => {
   let planId = req.query.id;
-  let plan = jsonData.overseas.filter(p => p.id = planId)[0];
+  let plan = jsonData.overseas.filter(p => p.id == planId)[0];
   if (!plan) {
-    plan = jsonData.domestic.filter(p => p.id = planId)[0];
+    plan = jsonData.domestic.filter(p => p.id == planId)[0];
   }
   res.json({plan});
 };
